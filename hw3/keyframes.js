7da40d103bede921cb1c7162f26830a176d20608
matthewHsieh <matthew19970221@gmail.com>
2021-04-29 14:49:29 +0800

    set work pos

--- hw3/keyframes.js
+++ hw3/keyframes.js
@@ -55,10 +55,10 @@ var TS2W = TWalk;
 
 //// walk-to-stand //////////
 
-var poseW2S0 = poseWalk1; // end of stand
+var poseW2S0 = poseStand0; // end of stand
 var poseW2S1 = {
-    lThigh: 0,
-    rThigh: 0
+    lThigh: -Math.PI / 6,
+    rThigh: Math.PI / 40 // right leg stays still
 }
 var poseW2S2 = poseStand0; // beginning of walk
 
@@ -129,8 +129,8 @@ function keyframeWalk(t, T) { // walk; periodic
 }
 
 function keyframeW2S(t, T) { // non-periodic
-    if (ts - t > T) { // end of walk
-        t = ts; // reset ts to start of stand
+    if (t - ts > T) { // end of walk
+        ts = t; // reset ts to start of stand
         state = 'Stand';
         // end of W2S: return last frame
         return [poseW2S2.lThigh, poseW2S2.rThigh];
@@ -138,7 +138,7 @@ function keyframeW2S(t, T) { // non-periodic
 
     // non-periodic walk-to-stand animation
     let keys = keysW2S;
-    var s = (ts - t) / T;
+    var s = (t - ts) / T;
 
     for (var i = 1; i < keys.length; i++) {
         if (keys[i][0] > s) break;
@@ -146,7 +146,9 @@ function keyframeW2S(t, T) { // non-periodic
     // take i-1
     var ii = i - 1;
     var a = (s - keys[ii][0]) / (keys[ii + 1][0] - keys[ii][0]);
-    intKey = [0, 0];
+    intKey = [keys[ii][1].lThigh * (1 - a) + keys[ii + 1][1].lThigh * a,
+        keys[ii][1].rThigh * (1 - a) + keys[ii + 1][1].rThigh * a
+    ];
     return intKey;
 
 }
